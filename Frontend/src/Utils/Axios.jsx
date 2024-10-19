4e1337fc47fe33b58a4787ba967c34c7d7d4c157
Anshul Gupta <anshulgupta8854@gmail.com>
2024-10-19 23:14:23 +0530

    first commit

--- Frontend/src/Utils/Axios.jsx
+++ Frontend/src/Utils/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api/v1/",
+  baseURL: "https://campustrade-backend.onrender.com/api/v1/",
   headers: {
     accept: "application/json",
     Authorization:
