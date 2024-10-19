ceb44f4ae65e38add95e75297b8fa2cdc238096f
Anshul Gupta <anshulgupta8854@gmail.com>
2024-10-20 00:14:30 +0530

    first commit

--- Backend/src/Controllers/user.controller.js
+++ Backend/src/Controllers/user.controller.js
@@ -130,8 +130,8 @@ const loginUser = asyncHandler(async (req, res) => {
   );
 
   const options = {
-    // httpOnly: true,
-    secure: true,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
   };
 
   const loggedInUser = await User.findById(user._id).select(
@@ -167,8 +167,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   );
 
   const options = {
-    // httpOnly: true,
-    secure: true,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
   };
 
   return res
