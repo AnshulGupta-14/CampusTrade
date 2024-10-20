299331413980fb5258180759b5534e2f94bab819
Anshul Gupta <anshulgupta8854@gmail.com>
2024-10-20 12:02:11 +0530

    first commit

--- Backend/src/Controllers/user.controller.js
+++ Backend/src/Controllers/user.controller.js
@@ -206,6 +206,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     const options = {
       httpOnly: true,
       secure: true,
+      sameSite: "None", // CSRF protection
     };
 
     const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
