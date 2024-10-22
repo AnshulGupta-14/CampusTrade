63882e04785eaca2ec62fe0d67247bd2d52e4c8c
Anshul Gupta <anshulgupta8854@gmail.com>
2024-10-22 21:03:28 +0530

    first commit

--- Backend/src/DB/index.js
+++ Backend/src/DB/index.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${process.env.DB_NAME}/?retryWrites=true&w=majority&appName=Cluster0`
+    );
     console.log(
       `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
     );
