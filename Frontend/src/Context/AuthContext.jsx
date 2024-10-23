f42b727b3dcd066e824671295c0ff6cb8b07f99a
Anshul Gupta <anshulgupta8854@gmail.com>
2024-10-23 19:24:21 +0530

    first commit

--- Frontend/src/Context/AuthContext.jsx
+++ Frontend/src/Context/AuthContext.jsx
@@ -4,11 +4,31 @@ import Cookies from "js-cookie";
 export const AuthContext = createContext();
 
 const AuthProvider = (props) => {
-  const [accessToken, setAccessToken] = useState(Cookies.get("accessToken"));
+  const [accessToken, setAccessToken] = useState(() => {
+    return Cookies.get("accessToken") || null;
+  });
+  // const updateAccessToken = (token) => {
+  //   setAccessToken(token);
+  //   if (token) {
+  //     localStorage.setItem("accessToken", token); // Save token to local storage
+  //   } else {
+  //     localStorage.removeItem("accessToken"); // Remove token
+  //   }
+  // };
+
+  useEffect(() => {
+    const token = Cookies.get("accessToken");
+    if (token) {
+      setAccessToken(token); // Update state if token exists
+    }
+  }, []);
+
   console.log(accessToken);
-  
+
   return (
-    <AuthContext.Provider value={{ accessToken, setAccessToken }}>
+    <AuthContext.Provider
+      value={{ accessToken, setAccessToken, updateAccessToken }}
+    >
       {props.children}
     </AuthContext.Provider>
   );
