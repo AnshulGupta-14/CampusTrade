5e1169d6d741b9103cb9bb10d817ac96725c0571
Anshul Gupta <anshulgupta8854@gmail.com>
2024-10-23 12:19:05 +0530

    first commit

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import Context from "./Context/Context.jsx";
+// import Context from "./Context/Context.jsx";
 import AuthProvider from "./Context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
