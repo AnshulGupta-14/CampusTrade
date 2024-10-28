94bfcb7bd35c5d28ae0a8d23640960219e282e0c
Anshul Gupta <anshulgupta8854@gmail.com>
2024-10-29 00:35:51 +0530

    first commit

--- Frontend/src/Components/UserProfile.jsx
+++ Frontend/src/Components/UserProfile.jsx
@@ -145,7 +145,7 @@ const UserProfile = () => {
 
       {!isDesktop && (
         <div className="pt-[22%] h-[100vh] flex flex-col relative">
-          <div className="w-full p-5 h-[20%] flex gap-10 items-center justify-center">
+          <div className="w-full p-5 h-[25%] flex gap-10 items-center justify-center">
             <img
               src={user.avatar}
               alt=""
@@ -189,7 +189,7 @@ const UserProfile = () => {
               ></Cards>
             </div>
           ) : (
-            <div className="w-full h-[70%] p-5 flex flex-col items-center justify-center">
+            <div className="w-full h-[65%] p-5 flex flex-col items-center justify-center">
               <img src="/Empty.png" alt="" className="w-1/2" />
               <h1 className="text-xl font-bold mt-7">
                 You haven't listed anything yet
