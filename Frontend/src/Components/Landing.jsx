a69574e22b7d47ceb4e3f5a354ab33157bfc1656
Anshul Gupta <anshulgupta8854@gmail.com>
2024-10-28 23:53:34 +0530

    first commit

--- Frontend/src/Components/Landing.jsx
+++ Frontend/src/Components/Landing.jsx
@@ -1,13 +1,15 @@
 import React, { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { gsap } from "gsap";
+import { useMediaQuery } from "react-responsive";
 
 const Landing = () => {
+  const isDesktop = useMediaQuery({ minWidth: 1224 });
   const navigate = useNavigate();
   const boxRef = useRef(null);
   const handleClick = async () => {
     await gsap.to(boxRef.current, {
-      x: -1200,
+      x: "-100%",
       duration: 2,
       opacity: 0,
     });
@@ -15,25 +17,54 @@ const Landing = () => {
   };
 
   return (
-    <div
-      ref={boxRef}
-      className="absolute z-50 w-screen h-screen flex flex-col items-center"
-    >
-      <div className="absolute top-[15%] text-center">
-        <h1 className="text-3xl font-bold">Welcome to CampusTrade!</h1>
-        <h1 className="mt-5">
-          Your one-stop marketplace for secondhand classics and rare finds.
-        </h1>
-        <h1>Here, each book has a story, waiting to be rediscovered by you.</h1>
-        <button
-          className="mt-5 px-5 py-2 bg-[#002f34] text-white rounded-xl"
-          onClick={handleClick}
+    <>
+      {isDesktop && (
+        <div
+          ref={boxRef}
+          className="absolute z-50 w-full h-full flex flex-col items-center"
         >
-          Get Started
-        </button>
-      </div>
-      <img src="/image.png" alt="" className="h-full w-full" />
-    </div>
+          <div className="absolute top-[15%] text-center">
+            <h1 className="text-3xl font-bold">Welcome to CampusTrade!</h1>
+            <h1 className="mt-5">
+              Your one-stop marketplace for secondhand classics and rare finds.
+            </h1>
+            <h1>
+              Here, each book has a story, waiting to be rediscovered by you.
+            </h1>
+            <button
+              className="mt-5 px-5 py-2 bg-[#002f34] text-white rounded-xl"
+              onClick={handleClick}
+            >
+              Get Started
+            </button>
+          </div>
+          <img src="/image.png" alt="" className="h-full w-full" />
+        </div>
+      )}
+
+      {!isDesktop && (
+        <div
+          ref={boxRef}
+          className="absolute z-50 w-full h-full flex flex-col items-center"
+        >
+          <div className="absolute top-[15%] mx-auto w-[70%] text-center">
+            <h1 className="text-2xl font-semibold">Welcome to CampusTrade!</h1>
+            <h1 className="mt-5">
+              Your one-stop marketplace for secondhand classics and rare finds.
+            </h1>
+            <h1>Here, each book has a story,</h1>
+            <h1>waiting to be rediscovered by you.</h1>
+          </div>
+          <img src="/image.png" alt="" className="h-full w-full" />
+          <button
+            className="absolute top-[80%] mt-5 px-5 py-2 bg-[#002f34] text-white rounded-xl"
+            onClick={handleClick} 
+          >
+            Get Started
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
