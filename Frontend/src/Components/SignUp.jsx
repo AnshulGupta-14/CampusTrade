01773735cb42cb9a291ef45d624fade4a40742e1
Anshul Gupta <anshulgupta8854@gmail.com>
2024-10-29 02:06:28 +0530

    first commit

--- Frontend/src/Components/SignUp.jsx
+++ Frontend/src/Components/SignUp.jsx
@@ -39,166 +39,170 @@ const SignUp = () => {
   };
 
   return (
-    <div className="w-full h-full pt-[6%] flex items-center justify-center text-[13px] font-semibold">
+    <div>
       {isDesktop && (
-        <div className="w-[30%] flex flex-col px-10 py-5 bg-white rounded-xl">
-          <h1 className="text-2xl font-bold ">Create Account</h1>
-          <h2 className="mt-3">Your Name</h2>
-          <input
-            className="border border-black p-2 py-1 text-md rounded-md"
-            type="text"
-            value={name}
-            onChange={(e) => {
-              setname(e.target.value);
-            }}
-          />
-          <h2 className="mt-3">Registration Number</h2>
-          <input
-            className="border border-black p-2 py-1 text-md rounded-md"
-            type="number"
-            value={reg}
-            onChange={(e) => {
-              setreg(e.target.value);
-            }}
-          />
-          <h2 className="mt-3">Email Id</h2>
-          <input
-            className="border border-black p-2 py-1 text-md rounded-md"
-            type="email"
-            value={email}
-            onChange={(e) => {
-              setemail(e.target.value);
-            }}
-          />
-          <h2 className="mt-3">Phone Number</h2>
-          <div className="flex gap-2">
-            <button className="border border-black p-2 py-1 text-md rounded-md">
-              +91
-            </button>
+        <div className="w-full h-screen pt-[6%] flex items-center justify-center text-[13px] font-semibold">
+          <div className="w-[30%] flex flex-col px-10 py-5 bg-white rounded-xl">
+            <h1 className="text-2xl font-bold ">Create Account</h1>
+            <h2 className="mt-3">Your Name</h2>
             <input
-              className="w-full border border-black p-2 py-1 text-md rounded-md"
-              type="tel"
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-              value={mobile}
+              className="border border-black p-2 py-1 text-md rounded-md"
+              type="text"
+              value={name}
               onChange={(e) => {
-                setmobile(e.target.value);
+                setname(e.target.value);
               }}
             />
+            <h2 className="mt-3">Registration Number</h2>
+            <input
+              className="border border-black p-2 py-1 text-md rounded-md"
+              type="number"
+              value={reg}
+              onChange={(e) => {
+                setreg(e.target.value);
+              }}
+            />
+            <h2 className="mt-3">Email Id</h2>
+            <input
+              className="border border-black p-2 py-1 text-md rounded-md"
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setemail(e.target.value);
+              }}
+            />
+            <h2 className="mt-3">Phone Number</h2>
+            <div className="flex gap-2">
+              <button className="border border-black p-2 py-1 text-md rounded-md">
+                +91
+              </button>
+              <input
+                className="w-full border border-black p-2 py-1 text-md rounded-md"
+                type="tel"
+                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                value={mobile}
+                onChange={(e) => {
+                  setmobile(e.target.value);
+                }}
+              />
+            </div>
+            <h2 className="mt-3">Username</h2>
+            <input
+              className="border border-black p-2 py-1 text-md rounded-md"
+              type="text"
+              value={username}
+              onChange={(e) => {
+                setusername(e.target.value);
+              }}
+            />
+            <h2 className="mt-3">Create Password</h2>
+            <input
+              className="border border-black p-2 py-1 text-md rounded-md"
+              type="text"
+              value={password}
+              onChange={(e) => {
+                setpassword(e.target.value);
+              }}
+            />
+            <h2 className="mt-3">Avatar</h2>
+            <input
+              className="border border-black p-2 py-1 text-md rounded-md"
+              type="file"
+              onChange={(e) => {
+                setavatar(e.target.files[0]);
+              }}
+              accept="image/*"
+            />
+            <input
+              className="rounded-full mt-7 mx-auto w-1/3 px-5 py-2 bg-[#fcd12d]"
+              type="submit"
+              value={"Register"}
+              onClick={submithandler}
+            />
           </div>
-          <h2 className="mt-3">Username</h2>
-          <input
-            className="border border-black p-2 py-1 text-md rounded-md"
-            type="text"
-            value={username}
-            onChange={(e) => {
-              setusername(e.target.value);
-            }}
-          />
-          <h2 className="mt-3">Create Password</h2>
-          <input
-            className="border border-black p-2 py-1 text-md rounded-md"
-            type="text"
-            value={password}
-            onChange={(e) => {
-              setpassword(e.target.value);
-            }}
-          />
-          <h2 className="mt-3">Avatar</h2>
-          <input
-            className="border border-black p-2 py-1 text-md rounded-md"
-            type="file"
-            onChange={(e) => {
-              setavatar(e.target.files[0]);
-            }}
-            accept="image/*"
-          />
-          <input
-            className="rounded-full mt-7 mx-auto w-1/3 px-5 py-2 bg-[#fcd12d]"
-            type="submit"
-            value={"Register"}
-            onClick={submithandler}
-          />
         </div>
       )}
 
       {!isDesktop && (
-        <div className="w-[80%] flex flex-col px-10 py-5 bg-white rounded-xl">
-          <h1 className="text-2xl font-bold ">Create Account</h1>
-          <h2 className="mt-3">Your Name</h2>
-          <input
-            className="border border-black p-2 py-1 text-md rounded-md"
-            type="text"
-            value={name}
-            onChange={(e) => {
-              setname(e.target.value);
-            }}
-          />
-          <h2 className="mt-3">Registration Number</h2>
-          <input
-            className="border border-black p-2 py-1 text-md rounded-md"
-            type="number"
-            value={reg}
-            onChange={(e) => {
-              setreg(e.target.value);
-            }}
-          />
-          <h2 className="mt-3">Email Id</h2>
-          <input
-            className="border border-black p-2 py-1 text-md rounded-md"
-            type="email"
-            value={email}
-            onChange={(e) => {
-              setemail(e.target.value);
-            }}
-          />
-          <h2 className="mt-3">Phone Number</h2>
-          <div className="flex gap-2">
-            <button className="border border-black p-2 py-1 text-md rounded-md">
-              +91
-            </button>
+        <div className="w-full h-[93vh] pt-[6%] flex items-center justify-center text-[13px] font-semibold">
+          <div className="w-[80%] flex flex-col px-10 py-5 bg-white rounded-xl">
+            <h1 className="text-2xl font-bold ">Create Account</h1>
+            <h2 className="mt-3">Your Name</h2>
             <input
-              className="w-full border border-black p-2 py-1 text-md rounded-md"
-              type="tel"
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-              value={mobile}
+              className="border border-black p-2 py-1 text-md rounded-md"
+              type="text"
+              value={name}
               onChange={(e) => {
-                setmobile(e.target.value);
+                setname(e.target.value);
               }}
             />
+            <h2 className="mt-3">Registration Number</h2>
+            <input
+              className="border border-black p-2 py-1 text-md rounded-md"
+              type="number"
+              value={reg}
+              onChange={(e) => {
+                setreg(e.target.value);
+              }}
+            />
+            <h2 className="mt-3">Email Id</h2>
+            <input
+              className="border border-black p-2 py-1 text-md rounded-md"
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setemail(e.target.value);
+              }}
+            />
+            <h2 className="mt-3">Phone Number</h2>
+            <div className="flex gap-2">
+              <button className="border border-black p-2 py-1 text-md rounded-md">
+                +91
+              </button>
+              <input
+                className="w-full border border-black p-2 py-1 text-md rounded-md"
+                type="tel"
+                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                value={mobile}
+                onChange={(e) => {
+                  setmobile(e.target.value);
+                }}
+              />
+            </div>
+            <h2 className="mt-3">Username</h2>
+            <input
+              className="border border-black p-2 py-1 text-md rounded-md"
+              type="text"
+              value={username}
+              onChange={(e) => {
+                setusername(e.target.value);
+              }}
+            />
+            <h2 className="mt-3">Create Password</h2>
+            <input
+              className="border border-black p-2 py-1 text-md rounded-md"
+              type="text"
+              value={password}
+              onChange={(e) => {
+                setpassword(e.target.value);
+              }}
+            />
+            <h2 className="mt-3">Avatar</h2>
+            <input
+              className="border border-black p-2 py-1 text-md rounded-md"
+              type="file"
+              onChange={(e) => {
+                setavatar(e.target.files[0]);
+              }}
+              accept="image/*"
+            />
+            <input
+              className="rounded-full mt-7 mx-auto w-1/3 px-5 py-2 bg-[#fcd12d]"
+              type="submit"
+              value={"Register"}
+              onClick={submithandler}
+            />
           </div>
-          <h2 className="mt-3">Username</h2>
-          <input
-            className="border border-black p-2 py-1 text-md rounded-md"
-            type="text"
-            value={username}
-            onChange={(e) => {
-              setusername(e.target.value);
-            }}
-          />
-          <h2 className="mt-3">Create Password</h2>
-          <input
-            className="border border-black p-2 py-1 text-md rounded-md"
-            type="text"
-            value={password}
-            onChange={(e) => {
-              setpassword(e.target.value);
-            }}
-          />
-          <h2 className="mt-3">Avatar</h2>
-          <input
-            className="border border-black p-2 py-1 text-md rounded-md"
-            type="file"
-            onChange={(e) => {
-              setavatar(e.target.files[0]);
-            }}
-            accept="image/*"
-          />
-          <input
-            className="rounded-full mt-7 mx-auto w-1/3 px-5 py-2 bg-[#fcd12d]"
-            type="submit"
-            value={"Register"}
-            onClick={submithandler}
-          />
         </div>
       )}
     </div>
