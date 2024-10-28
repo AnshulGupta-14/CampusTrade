fb3036cefa0041d3d1d987c25898cc6143607bfd
Anshul Gupta <anshulgupta8854@gmail.com>
2024-10-29 01:25:04 +0530

    first commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import Routing from "./Utils/Routing";
 import Nav from "./Components/Nav";
 import axios from "./Utils/Axios";
-import 'remixicon/fonts/remixicon.css'
+import "remixicon/fonts/remixicon.css";
+import { useMediaQuery } from "react-responsive";
 
 const App = () => {
+  const isDesktop = useMediaQuery({ minWidth: 1224 });
   const [products, setproducts] = useState([]);
 
   useEffect(() => {
@@ -24,10 +26,21 @@ const App = () => {
   }, []);
 
   return (
-    <div className="w-full min-h-screen bg-zinc-200">
-      <Nav data={products}></Nav>
-      <Routing></Routing>
-    </div>
+    <>
+      {isDesktop && (
+        <div className="w-full min-h-screen bg-zinc-200">
+          <Nav data={products}></Nav>
+          <Routing></Routing>
+        </div>
+      )}
+
+      {!isDesktop && (
+        <div className="w-full min-h-[93vh] bg-zinc-200">
+          <Nav data={products}></Nav>
+          <Routing></Routing>
+        </div>
+      )}
+    </>
   );
 };
 
