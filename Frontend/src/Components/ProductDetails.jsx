8f9b6277a4ff5a8dbe02685b16ecba01708bd7c7
Anshul Gupta <anshulgupta8854@gmail.com>
2024-11-07 17:15:06 +0530

    Modification

--- Frontend/src/Components/ProductDetails.jsx
+++ Frontend/src/Components/ProductDetails.jsx
@@ -131,7 +131,7 @@ const ProductDetails = () => {
       )}
 
       {!isDesktop && (
-        <div className="w-full flex flex-col ites-center mt-[26%] px-5 py-5">
+        <div className="w-full flex flex-col ites-center mt-[26%] px-5 py-5 gap-5">
           <div className="relative w-full bg-white h-[50vh] rounded-lg overflow-hidden">
             <div className="relative flex items-center justify-center h-[80%] w-full">
               {product.image && (
