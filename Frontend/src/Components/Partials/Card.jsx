93f72b096c759c850a6a0db25c7667ec01d74315
Anshul Gupta <anshulgupta8854@gmail.com>
2024-11-07 14:41:04 +0530

    Modification

--- Frontend/src/Components/Partials/Card.jsx
+++ Frontend/src/Components/Partials/Card.jsx
@@ -75,7 +75,7 @@ const Card = ({ data, close = false, onUpdate }) => {
       {isDesktop && (
         <NavLink
           to={`/productdetails/${data._id}`}
-          className="w-[22vw] h-[40vh] relative"
+          className="w-[22vw] h-[40vh] relative rounded-lg overflow-hidden"
           onMouseOver={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
         >
@@ -106,8 +106,8 @@ const Card = ({ data, close = false, onUpdate }) => {
             />
             <div className="h-full w-full absolute inset-0 bg-black opacity-30" />
           </div>
-          <div className="w-full h-[35%] bg-zinc-300 px-5 overflow-hidden">
-            <h1 className="text-xl font-bold leading-tight">&#8377;{data.price}</h1>
+          <div className="w-full h-[35%] bg-zinc-300 px-5 overflow-hidden leading-tight py-2">
+            <h1 className="text-xl font-bold">&#8377;{data.price}</h1>
             <p className="line-clamp-2 text-gray-600">{data.title}</p>
           </div>
           <div
