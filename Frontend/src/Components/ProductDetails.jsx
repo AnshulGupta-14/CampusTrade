95e535f6eeb6123649514c73c17fc5cad97c3664
Anshul Gupta <anshulgupta8854@gmail.com>
2024-11-07 17:58:01 +0530

    Modification

--- Frontend/src/Components/ProductDetails.jsx
+++ Frontend/src/Components/ProductDetails.jsx
@@ -137,13 +137,13 @@ const ProductDetails = () => {
           <div className="z-50">
             <button
               onClick={handlePrev}
-              className="absolute left-1 text-2xl p-2 bg-white rounded-full top-[35%]"
+              className="absolute left-1 text-2xl p-2 bg-white rounded-full top-[40%]"
             >
               <FaChevronLeft />
             </button>
             <button
               onClick={handleNext}
-              className="absolute right-1 text-2xl p-2 bg-white rounded-full top-[35%]"
+              className="absolute right-1 text-2xl p-2 bg-white rounded-full top-[40%]"
             >
               <FaChevronRight />
             </button>
