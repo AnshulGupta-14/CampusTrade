285bb8bde4144335d89eec7e547dc1eb14749b9f
Anshul Gupta <anshulgupta8854@gmail.com>
2024-11-07 14:18:06 +0530

    Modification

--- Frontend/src/Components/Partials/Card.jsx
+++ Frontend/src/Components/Partials/Card.jsx
@@ -108,7 +108,7 @@ const Card = ({ data, close = false, onUpdate }) => {
           </div>
           <div className="w-full h-[30%] bg-zinc-300 flex flex-col justify-center items-start px-5 py-3 overflow-hidden">
             <h1 className="text-xl font-bold">&#8377;{data.price}</h1>
-            <p className="text-gray-600">{data.title}</p>
+            <p className="line-clamp-2 text-gray-600">{data.title}</p>
           </div>
           <div
             className={`absolute inset-0 transition-opacity duration-300 ${
